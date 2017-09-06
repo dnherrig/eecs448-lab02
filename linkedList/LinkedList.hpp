74f4d2be8184ded431b5329ec2a9cfc5ac369d6f
Dylan Herrig <dylan.herrig@gmail.com>
2017-09-06 14:46:33 -0500

    Remove back function fixed

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -116,13 +116,29 @@ void LinkedList<T>::addFront(T value)
 template <typename T>
 bool LinkedList<T>::removeBack()
 {
-	Node<T>* lastNode = nullptr;
+	Node<T>* lastNode = m_front;
 	Node<T>* secondintoLast = nullptr;
 	bool isRemoved = false;
 
-	/** TODO 
-		Fix this method
-	*/
+	if(isEmpty())
+	{
+	}
+	else 
+	{
+		while((lastNode->getNext() != nullptr))
+		{
+			secondintoLast = lastNode;
+			lastNode = lastNode->getNext();
+		}
+
+		delete lastNode;
+		secondintoLast -> setNext(nullptr);
+ 
+		m_size--;
+		isRemoved = true;
+
+
+	}
 
 	return(isRemoved);
 }	
